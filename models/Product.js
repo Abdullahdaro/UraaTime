70438e09eab56ab4d9929d6f4b17f659c70a6831
AbdullahDaro <abode.morx@gmail.com>
2025-01-19 20:19:58 +0300

    new modul

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,16 @@ import mongoose, {model, Schema, models} from "mongoose";
 const ProductSchema = new Schema({
   title: {type:String, required:true},
   description: String,
+  details: {type:String},
+  included: [{type:String}],  // Array of items included
+  notIncluded: [{type:String}],  // Array of items not included
+  address: {
+    street: String,
+    city: String,
+    state: String,
+    zipCode: String,
+    country: String
+  },
   price: {type: Number, required: true},
   images: [{type:String}],
   category: {type:mongoose.Types.ObjectId, ref:'Category'},
